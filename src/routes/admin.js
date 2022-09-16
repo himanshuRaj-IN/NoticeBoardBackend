397ae0dd6160a0ef66c4cb1a0128e3d43b45b8a0
Himanshu Raj <himanshuraj9194@gmail.com>
2022-09-16 16:04:16 +0530

    clearing commit

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -11,7 +11,7 @@ router.post('/login', async (req, res, next) => {
         const userid = req.body.UserId;
         const user = await User.findOne({UserId:userid});
         if(!user) throw createError.NotFound('Not A Registered User')
-        if(user.Password !== req.body.Password) throw createError.Unauthorized('Either User Id/ Password is Not Valid')
+        if(user.Password !== req.body.Password) throw createError.Unauthorized('Either User Id or Password is Not Valid')
         const token = await signAccessToken(user.UserId);
         res.cookie("ACCESS_TOKEN" , token,{httpOnly:true, maxAge:600000});
         res.status(200).send({message : "Login Sucessfully"});
